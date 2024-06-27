72e8d957955cc54f0b4b58897c311007afba9f2e
Azure Way <87411939+karol-pieciukiewicz@users.noreply.github.com>
2024-06-27 22:20:31 +0200

    Upldate thumbprint for cloud foundry

---
+++ images/windows/scripts/build/Install-CloudFoundryCli.ps1
@@ -0,0 +1,26 @@
+################################################################################
+##  File:  Install-CloudFoundryCli.ps1
+##  Desc:  Install Cloud Foundry CLI
+################################################################################
+
+# Download the latest cf cli exe
+$cloudFoundryCliUrl = "https://packages.cloudfoundry.org/stable?release=windows64-exe&source=github"
+
+$cloudFoundryArchPath = Invoke-DownloadWithRetry $cloudFoundryCliUrl
+
+# Create directory for cf cli
+$cloudFoundryCliPath = "C:\cf-cli"
+New-Item -Path $cloudFoundryCliPath -ItemType Directory -Force
+
+# Extract the zip archive
+Write-Host "Extracting cf cli..."
+Expand-7ZipArchive -Path $cloudFoundryArchPath -DestinationPath $cloudFoundryCliPath
+
+# Add cf to path
+Add-MachinePathItem $cloudFoundryCliPath
+
+# Validate cf signature
+$cloudFoundrySignatureThumbprint = "2c6b2f1562698503a6e4a25f2df058e12e23a190"
+Test-FileSignature -Path "$cloudFoundryCliPath\cf.exe" -ExpectedThumbprint $cloudFoundrySignatureThumbprint
+
+Invoke-PesterTests -TestFile "CLI.Tools" -TestName "CloudFoundry CLI"
